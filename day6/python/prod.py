e7295da56f798ca3b15caa7741d68283150e53d1
Søren L. Hansen <sorenisanerd@gmail.com>
2022-12-05 21:07:28 -0800

    Day 6 done

---
+++ day6/python/prod.py
@@ -0,0 +1,28 @@
+def partA(filename: str) -> int:
+    lines = getLines(filename)
+    l = lines[0]
+    for i in range(0, len(l)-3):
+        if len(set(l[i:i+4])) == 4:
+            return i+4
+    return -1
+
+def partB(filename: str) -> int:
+    lines = getLines(filename)
+    l = lines[0]
+    for i in range(0, len(l)-13):
+        if len(set(l[i:i+14])) == 14:
+            return i+14
+    return -1
+
+def getLines(filename: str) -> list:
+    lines = []
+    with open(filename) as f:
+        for l in f:
+            l = l.rstrip('\n')
+            lines += [l]
+    return lines
+
+if __name__ == '__main__':
+    import os.path
+    print(partA(os.path.dirname(__file__) + '/../data/input.txt'))
+    print(partB(os.path.dirname(__file__) + '/../data/input.txt'))
