99685d88ee635beb87b0052150d5808ff62bfb36
Søren L. Hansen <sorenisanerd@gmail.com>
2022-12-13 22:42:02 -0800

    Apparently I forgot to commit day 10

---
+++ day10/python/prod.py
@@ -0,0 +1,53 @@
+def partA(filename: str) -> int:
+    lines = getLines(filename)
+    history = calculateHistory(lines)
+
+    counter = 0
+    for x in range(19, len(history), 40):
+        counter += (x+1)*history[x]
+
+    return counter
+
+def calculateHistory(lines):
+    history = [1]
+    for l in lines:
+        args = l.split(' ')
+        history += history[-1:]
+
+        if args[0] == 'noop':
+            continue
+
+        _, val = l.split()
+        history += [history[-1] + int(val)]
+    return history
+
+def partB(filename: str) -> int:
+    lines = getLines(filename)
+    history = calculateHistory(lines)
+
+    for h in range(len(history)):
+        sprite_position = history[h]
+        col = h % 40
+        if abs(col-sprite_position) < 2:
+            history[h] = '#'
+        else:
+            history[h] = '.'
+
+    for i in range(0, len(history), 40):
+        print(''.join(history[i:i+40]))
+
+
+def getLines(filename: str) -> list:
+    lines = []
+    with open(filename) as f:
+        for l in f:
+            l = l.rstrip('\n')
+            lines += [l]
+    return lines
+
+if __name__ == '__main__':
+    import os.path
+    print(partA(os.path.dirname(__file__) + '/../data/sample.txt'))
+    print(partA(os.path.dirname(__file__) + '/../data/input.txt'))
+    print(partB(os.path.dirname(__file__) + '/../data/sample.txt'))
+    print(partB(os.path.dirname(__file__) + '/../data/input.txt'))
